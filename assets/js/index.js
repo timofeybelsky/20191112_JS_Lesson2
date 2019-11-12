15f941cb7717d282dafcd315887a07f514ca4f71
Timofey <t1m0f3y@gmail.com>
2019-11-12 20:28:19 +0200

    Created calc v0.2 (switch)

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,7 @@ const secondValue = prompt("Enter secondValue:");
 
 const mark = prompt("Please enter opeartion mark:");
 
+/*
 if(mark === '+'){
   console.log(sumNumbers(firstValue, secondValue));
 }else if(mark === '-'){
@@ -15,6 +16,15 @@ if(mark === '+'){
   console.log(divideNumbers(firstValue, secondValue));
 }else{
   alert("ERROR!!! WTF!!!");
+}
+*/
+
+switch(mark){
+  case "+":{console.log(sumNumbers(firstValue, secondValue));} break;
+  case "-":{console.log(minusNumbers(firstValue, secondValue));} break;
+  case "*":{console.log(multipeNumbers(firstValue, secondValue));} break;
+  case "/":{console.log(divideNumbers(firstValue, secondValue));} break;
+  default:{alert("ERROR!!! WTF!!!");} break;
 }
 
 // ТУТЪ ФУНКЦИИ, БАРИН!
